da0d3d549ee6952987ae417ddbbde9702340c41b
hashim <hsk9988660@gmail.com>
2025-08-12 11:16:48 +0500

    simulate waiting time

--- testTask.py
+++ testTask.py
@@ -1,3 +1,5 @@
+import time
+
 class Task:
     def __init__(self, id, processing_time, waiting_time, user_tier):
         self.id = id
@@ -22,22 +24,24 @@ class PriorityQueue:
         if self.is_empty():
             return None
         task = self.queue.pop(0)
+
+        for t in self.queue:
+            t.waiting_time += task.processing_time 
+
         print(f"Processing Task {task.id} (User: {task.user_tier}, Waited: {task.waiting_time}ms, Time: {task.processing_time}ms)")
+        time.sleep(task.processing_time / 1000)  
         return task
 
     def is_empty(self):
         return len(self.queue) == 0
 
 
-# Create a priority queue
 task_queue = PriorityQueue()
 
-# Enqueue tasks
 task_queue.enqueue(Task(1, 2000, 1000, 'Free'))
 task_queue.enqueue(Task(2, 1500, 3000, 'Paid'))
 task_queue.enqueue(Task(3, 1000, 1000, 'Free'))
 task_queue.enqueue(Task(4, 1000, 1500, 'Paid'))
 
-# Process tasks
 while not task_queue.is_empty():
     task_queue.process_task()
